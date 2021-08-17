73fc57105261ff27508349fedf0b5ad19c2e829b
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-08-17 22:55:52 +0800

    init

--- sinking-websocket/sinking.go
+++ sinking-websocket/sinking.go
@@ -11,18 +11,18 @@ var upGrader = websocket.Upgrader{
 	},
 }
 
-type WebsocketHandle struct {
-	ErrorHandle     func(err error)
-	OnMessageHandle func(ws *websocket.Conn, messageType int, data []byte)
+type Websocket struct {
+	OnError   func(err error)
+	OnMessage func(ws *websocket.Conn, messageType int, data []byte)
 }
 
-func (handle *WebsocketHandle) SetErrorHandle(fun func(err error)) *WebsocketHandle {
-	handle.ErrorHandle = fun
+func (handle *Websocket) SetErrorHandle(fun func(err error)) *Websocket {
+	handle.OnError = fun
 	return handle
 }
 
-func (handle *WebsocketHandle) SetOnMessageHandle(fun func(ws *websocket.Conn, messageType int, data []byte)) *WebsocketHandle {
-	handle.OnMessageHandle = fun
+func (handle *Websocket) SetOnMessageHandle(fun func(ws *websocket.Conn, messageType int, data []byte)) *Websocket {
+	handle.OnMessage = fun
 	return handle
 }
 
@@ -35,26 +35,26 @@ func (err *Error) Error() string {
 	return err.ErrMsg
 }
 
-func (handle *WebsocketHandle) Listen(writer http.ResponseWriter, request *http.Request) {
+func (handle *Websocket) Listen(writer http.ResponseWriter, request *http.Request) {
 	defer func(writer http.ResponseWriter, request *http.Request) {
 		if request.Header.Get("Connection") != "Upgrade" {
-			if handle.ErrorHandle != nil {
-				handle.ErrorHandle(&Error{ErrCode: 500, ErrMsg: "Connection Close"})
+			if handle.OnError != nil {
+				handle.OnError(&Error{ErrCode: 500, ErrMsg: "Connection Close"})
 			}
 			return
 		}
 		ws, err := upGrader.Upgrade(writer, request, nil)
 		if err != nil {
-			if handle.ErrorHandle != nil {
-				handle.ErrorHandle(err)
+			if handle.OnError != nil {
+				handle.OnError(err)
 			}
 			return
 		}
 		defer func(ws *websocket.Conn) {
 			err := ws.Close()
 			if err != nil {
-				if handle.ErrorHandle != nil {
-					handle.ErrorHandle(err)
+				if handle.OnError != nil {
+					handle.OnError(err)
 				}
 				return
 			}
@@ -63,13 +63,13 @@ func (handle *WebsocketHandle) Listen(writer http.ResponseWriter, request *http.
 			//读取ws中的数据
 			mt, message, err := ws.ReadMessage()
 			if err != nil {
-				if handle.ErrorHandle != nil {
-					handle.ErrorHandle(err)
+				if handle.OnError != nil {
+					handle.OnError(err)
 				}
 				return
 			} else {
-				if handle.OnMessageHandle != nil {
-					handle.OnMessageHandle(ws, mt, message)
+				if handle.OnMessage != nil {
+					handle.OnMessage(ws, mt, message)
 				}
 			}
 		}
