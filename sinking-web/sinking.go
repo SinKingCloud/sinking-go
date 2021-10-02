fbc39dd75cf8d5c8aedb3409a89b136ca6d25815
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-10-02 18:15:10 +0800

    init

--- sinking-web/sinking.go
+++ sinking-web/sinking.go
@@ -31,12 +31,12 @@ func New() *Engine {
 	engine := &Engine{router: newRouter(), MaxMultipartMemory: defaultMultipartMemory}
 	engine.RouterGroup = &RouterGroup{engine: engine}
 	engine.groups = []*RouterGroup{engine.RouterGroup}
+	engine.Use(Recovery())
 	return engine
 }
 
 func Default() *Engine {
 	engine := New()
-	engine.Use(Logger(), Recovery())
 	return engine
 }
 
