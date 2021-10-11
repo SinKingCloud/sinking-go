ce7a7fa2bf1745d81d626de2df5beb397d639833
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-10-11 13:03:12 +0800

    init

--- sinking-consul/app/command/service/service.go
+++ sinking-consul/app/command/service/service.go
@@ -4,11 +4,14 @@ import (
 	"github.com/SinKingCloud/sinking-go/sinking-consul/app/util/logs"
 )
 
-func start() {
-	logs.Println("开始注册服务....")
-	logs.Println("注册服务成功....")
+func registerService() {
+	logs.Println("开始启动注册服务....")
+}
+
+func registerConfig() {
+	logs.Println("开始启动配置同步....")
 }
 
 func Run() {
-	go start()
+	go registerService()
 }
