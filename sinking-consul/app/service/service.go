ae5d5453f0ae9fe3f184f1139deee5e869998f23
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-10-11 22:52:26 +0800

    init

--- sinking-consul/app/service/service.go
+++ sinking-consul/app/service/service.go
@@ -3,7 +3,7 @@ package service
 import "github.com/SinKingCloud/sinking-go/sinking-consul/app/model"
 
 // Services 服务列表
-var Services = make(map[string]*Service)
+var Services = make(map[string]map[string]*Service)
 
 // Service 服务列表
 type Service struct {
@@ -11,9 +11,10 @@ type Service struct {
 	AppName       string         `json:"app_name"`        //所属应用
 	EnvName       string         `json:"env_name"`        //环境标识
 	GroupName     string         `json:"group_name"`      //分组名称
+	AppHash       string         `json:"app_hash"`        //标识hash(规则md5(AppName-EnvName-GroupName))
 	Ip            string         `json:"ip"`              //集群ip
 	Port          string         `json:"port"`            //集群端口
-	Hash          string         `json:"hash"`            //标识hash(规则md5(AppName-EnvName-GroupName-Ip:Port))
+	ServiceHash   string         `json:"service_hash"`    //标识hash(规则md5(AppName-EnvName-GroupName-Ip:Port))
 	LastHeartTime model.DateTime `json:"last_heart_time"` //上次心跳时间
 	Status        int            `json:"status"`          //服务状态(0:正常/1:异常)
 }
