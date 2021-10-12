200feed6449f97a5caa045f6f665a2b6870ce2d6
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-10-12 19:01:42 +0800

    init

--- sinking-consul/app/command/command.go
+++ sinking-consul/app/command/command.go
@@ -2,10 +2,8 @@ package command
 
 import (
 	"github.com/SinKingCloud/sinking-go/sinking-consul/app/command/service"
-	"github.com/SinKingCloud/sinking-go/sinking-consul/app/command/synchronized"
 )
 
 func Run() {
 	go service.Run()
-	go synchronized.Run()
 }
