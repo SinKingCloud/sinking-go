bb0d0d99ae9b3d8258dfe9bfe4d89dfb9c7fcea7
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-10-12 18:27:12 +0800

    init

--- sinking-consul/app/command/service/check.go
+++ sinking-consul/app/command/service/check.go
@@ -15,7 +15,12 @@ func checkCluster() {
 					service.Clusters[k].Status = 1
 				}
 			}
-			//检测服务
+			//检测服务状态
+			for k := range service.Services {
+				if service.Services[k].LastHeartTime+int64(setting.GetSystemConfig().Servers.CheckHeartTime) < time.Now().Unix() {
+					service.Services[k].Status = 1
+				}
+			}
 			time.Sleep(time.Duration(setting.GetSystemConfig().Servers.HeartTime) * time.Second)
 		}
 	}()
--- sinking-consul/app/http/controller/admin/service.go
+++ sinking-consul/app/http/controller/admin/service.go
@@ -8,5 +8,9 @@ import (
 
 // ServiceList 服务列表
 func ServiceList(s *sinking_web.Context) {
-	response.Success(s, "获取服务列表成功", service.Services)
+	var list []*service.Service
+	for _, v := range service.Services {
+		list = append(list, v)
+	}
+	response.Success(s, "获取服务列表成功", list)
 }
