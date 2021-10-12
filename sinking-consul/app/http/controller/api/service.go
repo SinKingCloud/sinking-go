915982437db8522598867391c352275a49a2e76c
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-10-12 17:13:14 +0800

    init

--- sinking-consul/app/http/controller/api/service.go
+++ sinking-consul/app/http/controller/api/service.go
@@ -8,8 +8,17 @@ import (
 // ServiceRegister 注册服务
 func ServiceRegister(s *sinking_web.Context) {
 	type register struct {
-		Ip   string `form:"ip" json:"ip"`
-		Port string `form:"port" json:"port"`
+		Name      string `form:"name" json:"name"`             //服务名称
+		AppName   string `form:"app_name" json:"app_name"`     //所属应用
+		EnvName   string `form:"env_name" json:"env_name"`     //环境标识
+		GroupName string `form:"group_name" json:"group_name"` //分组名称
+		Addr      string `form:"addr" json:"addr"`             //服务地址(规则ip:port)
+	}
+	form := &register{}
+	err := s.BindJson(&form)
+	if err != nil || form.Name == "" || form.AppName == "" || form.EnvName == "" || form.GroupName == "" || form.Addr == "" {
+		response.Error(s, "参数不足", nil)
+		return
 	}
 	response.Success(s, "注册服务成功", nil)
 }
--- sinking-consul/app/service/service.go
+++ sinking-consul/app/service/service.go
@@ -1,22 +1,17 @@
 package service
 
-import (
-	"time"
-)
-
 // Services 服务列表
-var Services = make(map[string][]map[string]*Service)
+var Services = make(map[string]map[string]*Service)
 
 // Service 服务列表
 type Service struct {
-	Name          string    `json:"name"`            //服务名称
-	AppName       string    `json:"app_name"`        //所属应用
-	EnvName       string    `json:"env_name"`        //环境标识
-	GroupName     string    `json:"group_name"`      //分组名称
-	AppHash       string    `json:"app_hash"`        //标识hash(规则md5(AppName-EnvName-GroupName))
-	Ip            string    `json:"ip"`              //集群ip
-	Port          string    `json:"port"`            //集群端口
-	ServiceHash   string    `json:"service_hash"`    //标识hash(规则md5(AppName-EnvName-GroupName-Ip:Port))
-	LastHeartTime time.Time `json:"last_heart_time"` //上次心跳时间
-	Status        int       `json:"status"`          //服务状态(0:正常/1:异常)
+	Name          string `json:"name"`            //服务名称
+	AppName       string `json:"app_name"`        //所属应用
+	EnvName       string `json:"env_name"`        //环境标识
+	GroupName     string `json:"group_name"`      //分组名称
+	AppHash       string `json:"app_hash"`        //标识hash(规则md5(AppName-EnvName-GroupName))
+	Addr          string `json:"addr"`            //服务地址(规则ip:port)
+	ServiceHash   string `json:"service_hash"`    //标识hash(规则md5(AppName-EnvName-GroupName-Addr))
+	LastHeartTime int64  `json:"last_heart_time"` //上次心跳时间
+	Status        int    `json:"status"`          //服务状态(0:正常/1:异常)
 }
