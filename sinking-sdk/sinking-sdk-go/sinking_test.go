70e604c5c5afe939181b27bb7ee7e8305966746f
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-10-14 18:03:27 +0800

    init

--- sinking-sdk/sinking-sdk-go/sinking_test.go
+++ sinking-sdk/sinking-sdk-go/sinking_test.go
@@ -8,15 +8,15 @@ import (
 )
 
 func Test_main(t *testing.T) {
-	server := New("106.52.89.187", "sinking-token", "test_token", "sinking-go-api-order", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8888")
+	server := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-order", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8888")
 	server.Listen()
-	server2 := New("106.52.89.187", "sinking-token", "test_token", "sinking-go-api-order", "sinking.go", "dev", "sinking-go-api", "106.52.89.187:80")
+	server2 := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-order", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8887")
 	server2.Listen()
-	server3 := New("106.52.89.187", "sinking-token", "test_token", "sinking-go-api-order", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8886")
+	server3 := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-order", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8886")
 	server3.Listen()
-	server4 := New("106.52.89.187", "sinking-token", "test_token", "sinking-go-api-pay", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8885")
+	server4 := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-pay", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8885")
 	server4.Listen()
-	server5 := New("106.52.89.187", "sinking-token", "test_token", "sinking-go-api-pay", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8884")
+	server5 := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-pay", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8884")
 	server5.Listen()
 	go func() {
 		time.Sleep(5 * time.Second)
