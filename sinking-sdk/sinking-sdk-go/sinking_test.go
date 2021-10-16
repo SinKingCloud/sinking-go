555da1ccf4de0a30bad6e30a32e67c4da1bf9ad8
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2021-10-16 15:35:28 +0800

    init

--- sinking-sdk/sinking-sdk-go/sinking_test.go
+++ sinking-sdk/sinking-sdk-go/sinking_test.go
@@ -2,13 +2,12 @@ package sinking_sdk_go
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
+	"net/http"
 	"testing"
 	"time"
 )
 
 var server *Register
-var serverConf *viper.Viper
 
 func Test_main(t *testing.T) {
 	//for i := 2080; i < 2080; i++ {
@@ -21,48 +20,14 @@ func Test_main(t *testing.T) {
 	server.Register("sinking-go-api", "sinking-go-api-order", "106.52.89.187").UseService(map[string]string{
 		"sinking-go-api": "sinking-go-api-order", //需要使用的服务
 	}).Listen()
+	time.Sleep(5 * time.Second) //延迟5s等待初始化完毕
 	//rpc调用服务
-	//body, err := server.Rpc("sinking-go-api-order").Timeout(5).Method(http.MethodPost).ReTry(5).Call("/index/login", &Param{
-	//	"user": "admin",
-	//	"pwd":  "123456",
-	//})
-	//fmt.Println(body, err)
-	//config拉取配置
-
-	for {
-		//fmt.Println(server.server)
-		//fmt.Println(services)
-		//body, err := server.Rpc("sinking-go-api-order").Timeout(5).Method(http.MethodPost).ReTry(5).Call("/index/login", &Param{
-		//	"user": "admin",
-		//	"pwd":  "123456",
-		//})
-		//fmt.Println(body, err)
-		serverConf = server.Config("sinking-go-api").Name("database").Viper()
-		fmt.Println(time.Now(), serverConf.Get("host"))
-		time.Sleep(time.Second)
-	}
-	//server.Listen()
-	//server2 := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-order", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8887")
-	//server2.Listen()
-	//server3 := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-order", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8886")
-	//server3.Listen()
-	//server4 := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-pay", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8885")
-	//server4.Listen()
-	//server5 := New("127.0.0.1:8888", "sinking-token", "test_token", "sinking-go-api-pay", "sinking.go", "dev", "sinking-go-api", "127.0.0.1:8884")
-	//server5.Listen()
-	//go func() {
-	//	time.Sleep(5 * time.Second)
-	//	for {
-	//		data, err := server.
-	//			Rpc("sinking-go-api-order").                          //调用服务名
-	//			Method(http.MethodPost).                              //请求方式
-	//			Header(map[string]string{"test": "test_data"}).       //请求头
-	//			Timeout(5).                                           //超时熔断
-	//			ReTry(5).                                             //最大重试次数
-	//			Call("/api/service/register", &Param{"test": "test"}) //调用地址及内容
-	//		fmt.Println(data, err)
-	//		time.Sleep(time.Second)
-	//	}
-	//}()
-	time.Sleep(999 * time.Second)
+	body, err := server.Rpc("sinking-go-api-order").Timeout(5).Method(http.MethodPost).ReTry(5).Call("/index/login", &Param{
+		"user": "admin",
+		"pwd":  "123456",
+	})
+	fmt.Println(body, err)
+	//获取配置
+	serverConf := server.Config("sinking-go-api").Name("database").Viper()
+	fmt.Println(serverConf.Get("host"))
 }
