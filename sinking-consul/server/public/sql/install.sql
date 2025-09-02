7e6b82b93efc09446fedfa7599cd423613c1707a
sinkingcloud <sinkingcloud@qq.com>
2025-09-02 18:21:57 +0800

    增加操作日志

--- sinking-consul/server/public/sql/install.sql
+++ sinking-consul/server/public/sql/install.sql
@@ -72,3 +72,28 @@ create index idx_node_online_status
 
 create index idx_node_status
     on cloud_nodes (status);
+
+create table cloud_logs
+(
+    id          integer           not null
+        constraint pk_id
+            primary key autoincrement,
+    type        integer default 0 not null,
+    ip          varchar(50),
+    title       varchar(200),
+    content     text,
+    update_time text,
+    create_time text
+);
+
+create index cloud_logs_createTime_index
+    on cloud_logs (create_time);
+
+create index cloud_logs_ip_index
+    on cloud_logs (ip);
+
+create index cloud_logs_type_index
+    on cloud_logs (type);
+
+create index cloud_logs_updateTime_index
+    on cloud_logs (update_time);
