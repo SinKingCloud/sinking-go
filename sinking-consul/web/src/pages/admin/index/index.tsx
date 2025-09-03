7fd77a94a97120f5975a43fe39344953b928d067
sinkingcloud <sinkingcloud@qq.com>
2025-09-03 16:16:06 +0800

    修正

--- sinking-consul/web/src/pages/admin/index/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Body} from "@/components";
-export default (): React.ReactNode => {
-    return (
-        <Body>
-           后台首页
-        </Body>
-    );
-};
--- sinking-consul/web/src/service/user/log.ts
+++
@@ -1,6 +0,0 @@
-import {API} from "../../../typings";
-import {post} from "@/utils/request";
-
-export async function getLogList(params: API.RequestParams = {}) {
-    return post("/user/person/log", params?.body, params?.onSuccess, params?.onFail, params?.onFinally);
-}
