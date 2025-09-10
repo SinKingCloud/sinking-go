6d4ffab8962aa0dfed96bc391cacfe93486d568f
sinkingcloud <sinkingcloud@qq.com>
2025-09-10 21:48:13 +0800

    修正

--- sinking-consul/web/config/config.ts
+++ sinking-consul/web/config/config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         defaultSettings?.favicons?.toString(),
     ],
     history: {
-        type: 'browser'
+        type: 'hash'
     },
     hash: true,
     exportStatic: {},
